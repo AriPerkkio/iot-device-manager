7dcbf7c27d07adfcd2fdfa96130c9e8b51adaa59
Ari Perkkiö <ari.perkkio@gmail.com>
2018-02-19 19:55:46 +0200

    DeviceRepository tests added

---
+++ src/test/java/web/repository/impl/DeviceRepositoryTest.java
@@ -0,0 +1,210 @@
+package web.repository.impl;
+
+import org.hamcrest.core.IsCollectionContaining;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+import web.domain.entity.Device;
+import web.repository.DeviceRepository;
+
+import java.util.Collection;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.junit.Assert.*;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DeviceRepositoryTest {
+
+    @Autowired
+    DeviceRepository deviceRepository;
+
+    /**
+     * Test add_device returns added device. Validate authentication key is generated
+     */
+    @Transactional
+    @Test
+    public void testAddDeviceReturnsAddedDeviceWithAuthenticationKey() {
+        // Given
+        Device expected = getTestDevice();
+
+        // When
+        Device result = deviceRepository.addDevice(expected);
+
+        // Then
+        assertThat(result.getName(), equalTo(expected.getName()));
+        assertNotNull(result.getAuthenticationKey());
+        assertThat(result.getAuthenticationKey().length(), equalTo(32));
+    }
+
+    /**
+     * Test add_device fails when device with duplicate name is used
+     */
+    @Transactional
+    @Test(expected = DataIntegrityViolationException.class)
+    public void testAddDeviceThrowsWhenNameNotUnique() {
+        // Given
+        Device device = getTestDevice();
+
+        // When
+        deviceRepository.addDevice(device);
+        deviceRepository.addDevice(device);
+    }
+
+    /**
+     * Test add_device fails when foreign key conflicts
+     */
+    @Transactional
+    @Test(expected = DataIntegrityViolationException.class)
+    public void testAddDeviceThrowsWhenForeignKeyConflicts() {
+        // Given
+        Device deviceWithConflict = getTestDevice();
+        deviceWithConflict.setConfigurationId(999);
+
+        // When
+        deviceRepository.addDevice(deviceWithConflict);
+    }
+
+    /**
+     * Test get_devices without parameters finds inserted device
+     */
+    @Transactional
+    @Test
+    public void testGetDevicesWithoutParametersReturnsInsertedDevice() {
+        // Given
+        Device expected = getTestDevice();
+
+        // When
+        deviceRepository.addDevice(expected);
+        Collection<Device> resultDevices = deviceRepository.getDevices(null, null, null, null, null, null);
+
+        // Then
+        assertThat(resultDevices, IsCollectionContaining.hasItem(hasProperty("name", equalTo(expected.getName()))));
+    }
+
+    /**
+     * Test get_devices with parameters finds inserted device
+     */
+    @Transactional
+    @Test
+    public void testGetDevicesWithParametersReturnsInsertedDevice() {
+        // Given
+        Device expected = deviceRepository.addDevice(getTestDevice());
+
+        // When
+        Collection<Device> resultDevices = deviceRepository.getDevices(expected.getId(), expected.getName(), expected.getDeviceTypeId(),
+            expected.getDeviceGroupId(), expected.getConfigurationId(), expected.getAuthenticationKey());
+
+        // Then
+        assertThat(resultDevices, IsCollectionContaining.hasItem(hasProperty("name", equalTo(expected.getName()))));
+    }
+
+    /**
+     * Test update device returns updated device
+     * Note: Not @Transactional due to update operation usage
+     */
+    @Test
+    public void testUpdateDeviceReturnsUpdatedDevice() {
+        // Given
+        Device initialDevice = getTestDevice();
+        Device expected = getTestDevice();
+        expected.setName("updated-name");
+
+        // When
+        deviceRepository.addDevice(initialDevice);
+        Device result = deviceRepository.updateDevice(null, initialDevice.getName(), null, expected);
+
+        // Then
+        assertThat(result.getName(), equalTo(expected.getName()));
+
+        // Update methods do not work as transactional - manual cleanup required
+        deviceRepository.deleteDevice(null, result.getName(), null);
+    }
+
+    /**
+     * Test update device fails when foreign key conflicts
+     */
+    @Test(expected = DataIntegrityViolationException.class)
+    @Transactional
+    public void testUpdateDeviceThrowsWhenForeignKeyConflicts() {
+        // Given
+        Device device = getTestDevice();
+        Device deviceWithConflict = getTestDevice();
+        deviceWithConflict.setConfigurationId(999);
+
+        // When
+        deviceRepository.addDevice(device);
+        deviceRepository.updateDevice(null, device.getName(), null, deviceWithConflict);
+    }
+
+    /**
+     * Test update device fails when unique key conflicts
+     */
+    @Transactional
+    @Test(expected = DataIntegrityViolationException.class)
+    public void testUpdateDeviceThrowsWhenNameNotUnique() {
+        // Given
+        Device initialDevice = getTestDevice();
+        Device updateDevice = getTestDevice();
+        updateDevice.setName("non-conflicting-name");
+
+        // When
+        deviceRepository.addDevice(initialDevice);
+        deviceRepository.addDevice(updateDevice);
+        deviceRepository.updateDevice(null, updateDevice.getName(), null, initialDevice);
+    }
+
+    /**
+     * Test delete_device with parameters deletes devices
+     */
+    @Transactional
+    @Test
+    public void testDeleteDeviceWithParametersWorks() {
+        // Given
+        Device device = getTestDevice();
+
+        // When
+        deviceRepository.addDevice(device);
+        Collection<Device> resultsBefore = deviceRepository.getDevices(null, null, null, null, null, null);
+        Boolean result = deviceRepository.deleteDevice(null, device.getName(), null);
+        Collection<Device> resultsAfter = deviceRepository.getDevices(null, null, null, null, null, null);
+
+        //Then
+        assertTrue(result);
+        assertThat(resultsBefore.size(), equalTo(1));
+        assertThat(resultsAfter.size(), equalTo(0));
+    }
+
+    /**
+     * Test delete_device without parameters doesn't delete anything
+     */
+    @Transactional
+    @Test
+    public void testDeleteDeviceWithoutParametersWontDeleteAll() {
+        // Given
+        Device device = getTestDevice();
+
+        // When
+        deviceRepository.addDevice(device);
+        Collection<Device> resultsBefore = deviceRepository.getDevices(null, null, null, null, null, null);
+        Boolean result = deviceRepository.deleteDevice(null, null, null);
+        Collection<Device> resultsAfter = deviceRepository.getDevices(null, null, null, null, null, null);
+
+        //Then
+        assertFalse(result);
+        assertThat(resultsBefore.size(), equalTo(1));
+        assertThat(resultsAfter.size(), equalTo(1));
+    }
+
+    private Device getTestDevice() {
+        Device device = new Device();
+        device.setName("test-device");
+        return device;
+    }
+}
